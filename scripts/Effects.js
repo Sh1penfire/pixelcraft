c0152c59d689b88f702aaaa33bd53b7c2606eee5
Pyratite <73347888+Pyratite@users.noreply.github.com>
2020-10-24 00:32:17 +1100

    Add files via upload

---
+++ scripts/Effects.js
@@ -0,0 +1,7 @@
+const healedStatus = extendContent(StatusEffect, "healed");
+healedStatus.speedMultiplier = 2;
+healedStatus.armorMultiplier = .5;
+healedStatus.damage = -1;
+healedStatus.effect = healBlockFull;
+healedStatus.colour  = green;
+                                                            
