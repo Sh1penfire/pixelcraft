b493e7f8ef5425c62ff9a4867bebf82a02956b49
Pyratite <73347888+Pyratite@users.noreply.github.com>
2020-10-25 16:08:35 +1100

    Trying to create a status effect

--- scripts/Effects.js
+++ scripts/Effects.js
@@ -1,7 +1,13 @@
-const healedStatus = extendContent(StatusEffect, "healed");
+
+const healedStatus = extendContent(StatusEffect, "healed", {});
 healedStatus.speedMultiplier = 2;
 healedStatus.armorMultiplier = .5;
 healedStatus.damage = -1;
-healedStatus.effect = healBlockFull;
-healedStatus.colour  = green;
+healedStatus.effect = healedStatusFX;
+healedStatus.colour  = Color.green;
                                                             
+const healedStatusFX = newEffect(24, e => {
+	Draw.color(Color.valueOf("#639d2a").shiftHue(Mathf.random(100)), Color.valueOf("#639d2a").shiftHue(Mathf.random(200)), Mathf.random());
+	Draw.mixcol(Color.valueOf("#639d2a"), Mathf.random());
+    Fill.circle(e.x, e.y, e.fout() * 2 * Mathf.random());
+});
