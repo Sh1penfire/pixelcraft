42d92d1623e8746f04ef7f966abffc7abaa4ae5f
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-08 11:06:13 +1100

    Update Effects.js

--- scripts/Effects.js
+++ scripts/Effects.js
@@ -1,8 +1,6 @@
 
-const healedStatus = extendContent(StatusEffect, "healed", {	
-	init(){
-	}
-});
+const healedStatus = extendContent(StatusEffect, "healed", {});
+
 healedStatus.speedMultiplier = 2;
 healedStatus.armorMultiplier = .5;
 healedStatus.damage = -1;
@@ -10,7 +8,7 @@ healedStatus.effect = healedStatusFX;
 healedStatus.colour  = Color.green;
                                                             
 const healedStatusFX = newEffect(24, e => {
-	Draw.color(Color.valueOf("#639d2a").shiftHue(Mathf.random(100)), Color.valueOf("#639d2a").shiftHue(Mathf.random(200)), Mathf.random());
-	Draw.mixcol(Color.valueOf("#639d2a"), Mathf.random());
-    Fill.circle(e.x, e.y, e.fout() * 2 * Mathf.random());
+Draw.color(Color.yellow, Color.green, e.fin());
+Lines.stroke(e.fin() * 1);
+Lines.circle(e.x, e.y, e.fslope() * 1);
 });
