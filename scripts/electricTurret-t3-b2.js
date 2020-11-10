83bb3187b37cbc81cc6bcc054839d15bcf5113c2
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-10 21:25:46 +1100

    Update electricTurret-t3-b2.js

--- scripts/electricTurret-t3-b2.js
+++ scripts/electricTurret-t3-b2.js
@@ -35,7 +35,7 @@ const shot = extend(ArtilleryBulletType, {});
 const blastShot = extend(BasicBulletType, {});
 
 //extends off the puver hjson file
-const electricTurrett3b2 = extendContent(PowerTurret, "electricTurrett3b2" {
+const puver = extendContent(PowerTurret, "puver" {
   icons(){
     return [
       Core.atlas.find("block-3"),
@@ -47,9 +47,9 @@ const electricTurrett3b2 = extendContent(PowerTurret, "electricTurrett3b2" {
 //givving things stats
 
 //givving puver it's stats (Some are predefined in the puver.hjson file
-electricTurrett3b2.recoil = 1;
-electricTurrett3b2.restitution = 0.015;
-electricTurrett3b2.shootType = shot;
+puver.recoil = 1;
+puver.restitution = 0.015;
+puver.shootType = shot;
 
 //stats of bullet shot by puver
 shot.damage = 15;
