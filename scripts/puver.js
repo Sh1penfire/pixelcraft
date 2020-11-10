6de6194e4ca28feb2c9feea7dc4ecfae949fbed2
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-10 17:30:02 +1100

    Update puver.js

--- scripts/puver.js
+++ scripts/puver.js
@@ -1,5 +1,11 @@
 // Obligatory comment line for no reason at all
-
+//Put effects at the top of the script so they load in first.
+const puverShoot = new Effect(40, e => {
+    Draw.color( Color.white, Color.black, e.fout());
+    Lines.stroke(e.fin() *2); 
+    Lines.circle(e.x, e.y, e.fout() * 25 * Mathf.random()); 
+});
+//then put the bullets after the effects so they load before the turret
 const blastShot = extend(BasicBulletType, {});
 blastShot.damage = 3;
 blastShot.speed = 3;
@@ -12,12 +18,8 @@ blastShot.despawnEffect = Fx.none;
 blastShot.shootEffect = Fx.puverShoot;
 blastShot.smokeEffect = Fx.none;
 
-const puverShoot = new Effect(40, e => {
-    Draw.color( Color.white, Color.black, e.fout());
-    Lines.stroke(e.fin() *2); 
-    Lines.circle(e.x, e.y, e.fout() * 25 * Mathf.random()); 
-});
 
+//then laod the turret
 const puver = extendContent(PowerTurret, "puver", {
   icons(){
     return [
