0784e440312f7d42e1b6c56f1e6b1fcee3bdabd1
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-10 19:37:53 +1100

    Update puver.js

--- scripts/puver.js
+++ scripts/puver.js
@@ -1,20 +1,33 @@
 // Obligatory comment line for no reason at all
 
+//shoot effect of puver
 const puverShoot = new Effect(40, e => {
   Draw.color(Color.black, Color.white, e.fin());
   Lines.stroke(e.fout() * 2);
   Lines.circle(e.x, e.y, e.fin() * 10);
 });
 
+//effect when bullet breaks
+const shotHit = new Effect(40, e => {
+  Draw.color(Color.black, Color.orange, e.fin());
+  Lines.stroke(e.fout() * 2);
+  Fill.circle(e.x, e.y, e.fin() * 7);
+});
 
-const blastDespawn = new Effect(40, e => {
+//frag effect
+const blast = new Effect(40, e => {
   Draw.color(Color.black, Color.orange, e.fin());
   Lines.stroke(e.fin() * 2);
   Lines.circle(e.x, e.y, e.fout() * 7);
 });
 
+//makes the shot of puver
+const shot = extend(BasicBulletType, {});
+
+//makes frag bullets
 const blastShot = extend(BasicBulletType, {});
 
+//extends off the puver hjson file
 const puver = extendContent(PowerTurret, "puver", {
   icons(){
     return [
@@ -23,11 +36,30 @@ const puver = extendContent(PowerTurret, "puver", {
     ];
   }
 });
+
+//givving things stats
+
+//givving puver it's stats (Some are predefined in the puver.hjson file
 puver.recoil = 1;
 puver.restitution = 0.015;
-puver.shootType = blastShot;
+puver.shootType = shot;
 
-blastShot.damage = 3;
+//stats of bullet shot by puver
+shot.damage = 15;
+shot.speed = 3;
+shot.lifetime = 35;
+shot.knockback = 5;
+shot.width = 3;
+shot.height = 5;
+shot.collides = true;
+shot.collidesTiles = true;
+shot.hitEffect = shotHit;
+shot.despawnEffect = FX.none;
+shot.shootEffect = puverShoot;
+shot.smokeEffect = Fx.none;
+
+//now stats of frag bullet
+blastShot.damage = 5;
 blastShot.speed = 3;
 blastShot.lifetime = 35;
 blastShot.knockback = 5;
@@ -35,7 +67,7 @@ blastShot.width = 3;
 blastShot.height = 5;
 blastShot.collides = true;
 blastShot.collidesTiles = true;
-blastShot.hitEffect = blastDespawn;
-blastShot.despawnEffect = blastDespawn
+blastShot.hitEffect = blast;
+blastShot.despawnEffect = blast;
 blastShot.shootEffect = puverShoot;
 blastShot.smokeEffect = Fx.none;
