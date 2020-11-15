c5f9748d3714a8398a91e9dc3a135d122bda0849
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-15 20:44:17 +1100

    Update helgravator.js

--- scripts/helgravator.js
+++ scripts/helgravator.js
@@ -5,7 +5,15 @@ flPixelite.damage = 30;
 flPixelite.splashDamageRadius = 16;
 flPixelite.width = 12;
 flPixelite.height = 24;
-Vars.content.getByName(ContentType.block,"pixelcraft-flamethrower3").put(Vars.content.getByName(ContentType.item,"pixelcraft-pixelite"),flPixelite);
+const helgravator = extendContent(ItemTurret, "flamethrower3"){
+	  icons(){
+    return [
+      Core.atlas.find("block-2"),
+      Core.atlas.find("pixelcraft-flamethrower1")
+    ];
+  }
+});
+this.ammo(Vars.content.getByName(ContentType, "pixelcraft-pixelite"), flPixelite);
 
 
 	//Blocks.flamethrower3.put(Items.pyratite, flPyratite)
