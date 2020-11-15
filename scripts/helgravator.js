20ef500049659d8ceda02733a0944d8f0f3a7507
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-15 19:08:46 +1100

    Update helgravator.js

--- scripts/helgravator.js
+++ scripts/helgravator.js
@@ -5,9 +5,7 @@ flPixelite.damage = 30;
 flPixelite.splashDamageRadius = 16;
 flPixelite.width = 12;
 flPixelite.height = 24;
-init(){
-	this.super$init();
-	Blocks.flamethrower3.put(Vars.content.getByName(ContentType.item,"pixelcraft-pixelite"),flPixelite);
-}
+Blocks.flamethrower3.put(Vars.content.getByName(ContentType.item,"pixelcraft-pixelite"),flPixelite);
+
 
 	//Blocks.flamethrower3.put(Items.pyratite, flPyratite)
