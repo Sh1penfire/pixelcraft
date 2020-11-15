00735cff16aa1132500839fcb31648b0b8463696
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-15 17:05:11 +1100

    Update ioniser.js

--- scripts/ioniser.js
+++ scripts/ioniser.js
@@ -1,5 +1,5 @@
 
-const ioniser = extendContent(ChargeTurret, "electricTurret4b1", {
+const ioniser = extendContent(PowerTurret, "electricTurret4b1", {
   icons(){
     return [
       Core.atlas.find("block-4"),
