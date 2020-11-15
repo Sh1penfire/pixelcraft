2f78c3b5c4afe616828445ae21e08a006957d38f
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-15 16:20:40 +1100

    Create ioniser.js

---
+++ scripts/ioniser.js
@@ -0,0 +1,8 @@
+const ioniser = extendContent(ChargeTurret, "electricTurret4b1", {
+  icons(){
+    return [
+      Core.atlas.find("block-4"),
+      Core.atlas.find("pixelcraft-healingTurret2")
+    ];
+  }
+});
