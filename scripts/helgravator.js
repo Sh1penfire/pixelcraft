8f8a92fc2380fa1e7f42ffe5aaff2018a5fd821a
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-16 20:01:53 +1100

    Update helgravator.js

--- scripts/helgravator.js
+++ scripts/helgravator.js
@@ -2,6 +2,9 @@ const hellfireFX = new Effect(20, e => {
 Draw.color(Color.orange, Color.red, e.fin());
 Fill.circle(e.x, e.y, e.fslope() * 4);
 Fill.circle(e.x, e.y, e.fout() * 2);
+Draw.color(Color.orange, Color.red, e.fin());
+Lines.stroke(e.fslope() * 2);
+Lines.circle(e.x, e.y, e.fin() * 6);
 });
 
 const firehitFx = new Effect(40, e => {
@@ -24,7 +27,7 @@ hellfire.color = Color.white;
 
 const flCoal = extend(BasicBulletType, {});
 flCoal.speed = 20;
-flCoal.damage = 25;
+flCoal.damage = 10;
 flCoal.width = 1;
 flCoal.height = 1;
 flCoal.innacuracy = 15;
@@ -35,6 +38,23 @@ flCoal.despawnEffect = Fx.none;
 flCoal.hitEffect = firehitFx;
 flCoal.status = hellfire;
 flCoal.ammoMultiplier = 10;
+flCoal.pierce = true;
+
+const flPixelite = extend(BasicBulletType, {});
+flPixelite.speed = 20;
+flPixelite.damage = 15;
+flPixelite.width = 1;
+flPixelite.height = 1;
+flPixelite.innacuracy = 15;
+flPixelite.lifetime = 5;
+flPixelite.shootSound = Sounds.flame2;
+flPixelite.shootEffect = Fx.fire;
+flPixelite.despawnEffect = Fx.none;
+flPixelite.hitEffect = firehitFx;
+flPixelite.status = hellfire;
+flPixelite.ammoMultiplier = 10;
+flPixelite.pierce = true;
+
 const helgravator = extendContent(ItemTurret, "flamethrower3",{
   init(){
     this.ammo(Vars.content.getByName(ContentType.item,"coal"), flCoal);
