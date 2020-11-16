a53167f9adac8395c9867bcacd4386712e2cfdbf
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-11-16 15:24:49 +1100

    Update helgravator.js

--- scripts/helgravator.js
+++ scripts/helgravator.js
@@ -1,3 +1,18 @@
+const hellfireFX = new Effect(5, e => {
+Draw.color(Color.orange, Color.red, e.fin());
+ Draw.alpha(e.fslope + 0.5());
+Fill.circle(e.x, e.y, e.fslope() * 5);
+});
+
+const hellfire = extendContent(StatusEffect, "hellfire", {});
+
+hellfire.speedMultiplier = 0.8;
+hellfire.armorMultiplier = 0.75;
+hellfire.damage = .5;
+hellfire.effect = hellfireFX;
+hellfire.color  = Color.white;
+
+
 const flPixelite = extend(BasicBulletType, {});
 flPixelite.speed = 20;
 flPixelite.splashDamage = 5;
@@ -11,6 +26,7 @@ flPixelite.shootSound = Sounds.flame2;
 flPixelite.shootEffect = Fx.shootPyraFlame;
 flPixelite.despawnEffect = Fx.none;
 flPixelite.hitEffect = Fx.fire;
+flPixelite.status = hellfire;
 const helgravator = extendContent(ItemTurret, "flamethrower3",{
   init(){
     this.ammo(Vars.content.getByName(ContentType.item,"pixelcraft-pixelite"), flPixelite);
