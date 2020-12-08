9cf2018ed2c90f34b81371df1a9dad8e60fc09cb
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-12-08 11:35:04 +1100

    Small update
    -Gave bionorbs usage in a turret
    -Coal compresser is now graphite press but faster
    -Spread turret is now unlockable after string
    -nerfed biomender

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,34 @@ var floodedVally = SectorPreset("floodedVally", Planets.serpulo, 206);
 floodedVally.localizedName = "Flooded Vally";
 floodedVally.difficulty = 10;
 floodedVally.alwaysUnlocked = true;
+
+const landfill = extendContent(Wall, "landfill", {
+  icons() {
+    return [Core.atlas.find(this.modName + this.name)];
+  },
+  canPlaceOn(tile) {
+    return (
+      tile != null && (tile.floor().isDeep() || tile.floor().liquidDrop != null)
+    );
+  },
+});
+
+/*
+landfill.size = 1;
+landfill.health = 10;
+landfill.floating = true;
+landfill.category = Category.effect;
+landfill.buildVisibility = BuildVisibility.shown;
+
+landfill.buildType = () => 
+  extendContent(Wall.WallBuild, landfill, {
+    placed() {
+      var tile = Vars.world.tile(this.tileX(), this.tileY());
+      tile.setBlock(Blocks.air);
+      tile.setFloor(Blocks.dirt);
+      Vars.renderer.minimap.reset();
+      Vars.renderer.minimap.updateAll();
+      Vars.renderer.blocks.floor.clearTiles();
+    },
+  });
+*/
