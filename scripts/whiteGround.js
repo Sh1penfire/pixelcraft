684e8a5f93cf7a240563268378b6606ffc29ba72
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2020-12-09 23:49:41 +1100

    Small update
    -Changed how biomenders work slightly
    -Added new unit which is available in sandbox play
    -added ioniser
    -We now have another soda can *Sparkeling water*

--- scripts/whiteGround.js
+++ scripts/whiteGround.js
@@ -1,26 +1,55 @@
-const warmth = extend(StatusEffect, "warmth", {
-    init(){
-                this.opposite(StatusEffects.melting, StatusEffects.burning);
-        /*
-                this.trans(StatusEffects.freezing, this.unit, this.time, this.newTime, result){
-                    unit.heal(1)
-                    result.set(this, time)
-                };
-                //no idea hwo to fix this, whenever I leave it uncommented, it just screws it over. Missing ; on the line before (4#)
-                */
-                
-}
-});
-warmth.speedMultiplier = 1.66666666667;
-warmth.healthMultiplier = 1.2;
-warmth.damageMultiplier = 1.25;
-warmth.damage = 0.0;
-warmth.effect = Fx.none;
-warmth.color = Color.white;
+const warmth = extend(StatusEffect, "warmth", {});
+warmth.speedMultiplier = 1.5;
+warmth.healthMultiplier = 1.15;
+warmth.reloadMultiplier = 1.15;
+warmth.effect = Fx.freezing;
+warmth.effectChance = 0.07;
+
+const fragment = extend(BasicBulletType, {});
+fragment.damage = 5;
+fragment.frontColor = Color.valueOf("b6cad6");
+fragment.backColor = Color.valueOf("d7e0e5");
+fragment.hitEffect = Fx.hitLancer;
+fragment.despawnEffect = Fx.freezing;
+
+const fragmentBomb = extend(BombBulletType, {});
+fragmentBomb.splashDamageRadius = 0;
+fragmentBomb.splashDamage = 0;
+fragmentBomb.width = 0;
+fragmentBomb.height = 0;
+fragmentBomb.lifetime = 0;
+fragmentBomb.status = StatusEffects.freezing;
+fragmentBomb.despawnEffect = Fx.none;
+fragmentBomb.hitEffect = Fx.none;
+fragmentBomb.hitSound = Sounds.none;
+fragmentBomb.fragBullets = 4;
+fragmentBomb.fragBullet = fragment;
+
+const cryFac = extendContent(UnitFactory, "cryFac", {});
 
 const crystal = extendContent(UnitType, "crystal", {});
 crystal.constructor = () => extend(MechUnit, {
+    killed(){     
+        Fx.explosion.at(this.x, this.y)
+        fragmentBomb.create(this, this.team, this.x, this.y, Mathf.random(360), Mathf.random(0));
+        this.dead = true;
+    }
 });
-crystal.abilities.add(new StatusFieldAbility(StatusEffects.freezing, 3600, 360, 60));
-crystal.abilities.add(new StatusFieldAbility(warmth, 3600, 360, 60));
+crystal.abilities.add(new StatusFieldAbility(StatusEffects.freezing, 360, 360, 60));
+crystal.abilities.add(new StatusFieldAbility(warmth, 360, 360, 60));
 
+
+Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(crystal, 60 * 8, ItemStack.with(Items.silicon, 25, Items.lead, 15, Items.metaglass, 15)));
+
+const stalactite = extendContent(UnitType, "stalactite", {});
+stalactite.constructor = () => extend(MechUnit, {
+    killed(){     
+        Fx.explosion.at(this.x, this.y)
+        fragmentBomb.create(this, this.team, this.x, this.y, Mathf.random(360), Mathf.random(0));
+        this.dead = true;
+    }
+});
+stalactite.abilities.add(new StatusFieldAbility(StatusEffects.freezing, 360, 360, 60));
+stalactite.abilities.add(new StatusFieldAbility(warmth, 360, 360, 60));
+
+Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(stalactite, 60 * 8, ItemStack.with(Items.silicon, 25, Items.lead, 15, Items.metaglass, 16)));
