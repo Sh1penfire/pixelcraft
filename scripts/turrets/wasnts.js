d2692b0dd0b7ff1769ba7e33ff697859631b4f1f
QmelZ <59574967+QmelZ@users.noreply.github.com>
2020-12-31 06:22:46 +0300

    destory

--- scripts/turrets/wasnts.js
+++ scripts/turrets/wasnts.js
@@ -1,15 +1,5 @@
 // this is what i could get from project unity's code -QmelZ
-const colors = [
-    "2700ff", "8f00ff", "80ff00", "006b80", "66aa0b", "00ffcc", "91c4ff", "cbd97f", "ffaa5f"
-];
-
-const endgame = new Effect(32, e => {
-    Draw.color(Color.valueOf(colors[Mathf.round(Mathf.random() * 8)]));
-    Lines.stroke(1.2);
-    Lines.line(e.x, e.y, e.data.x, e.data.y);
-});
-
-const wasnts = extend(Block, "wasnts", {
+const endgame = extend(Block, "wasnts", {
     localizedName: "wasnt's",
     description: "never was.",
     buildVisibility: BuildVisibility.shown,
@@ -27,13 +17,29 @@ const wasnts = extend(Block, "wasnts", {
     }
 });
 
-wasnts.buildType = () => extend(Building, {
+const colors = [
+    "2700ff", "8f00ff", "80ff00", "006b80", "66aa0b", "00ffcc", "91c4ff", "cbd97f", "ffaa5f"
+];
+
+endgame.buildType = () => extend(Building, {
     updateTile(){
-        Groups.unit.each(u => {
-            if(u.team != this.team){
-                endgame.at(this.x, this.y, 0, u);
-                u.destroy();
+        Groups.unit.each(e => {
+            if(e.team != this.team){
+                e.destroy();
             };
+        });
+        Groups.build.each(e => {
+            if(e.team != this.team){
+                e.kill();
+            };
+        });
+        Groups.bullet.each(e => {
+            if(e.team != this.team){
+                e.remove();
+            };
+        });
+        Groups.fire.each(e => {
+            e.remove();
         });
     }
 });
