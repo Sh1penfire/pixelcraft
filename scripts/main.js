6ce23594acc268d13d36aeca5ae2c1ac55e7e228
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2021-01-01 14:41:22 +1100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -29,6 +29,7 @@ const fireSpawn = extend(StatusEffect, "fireSpawn", {
   update(unit, time){
   if(Mathf.chance(0.1 * Time.delta)){
     this.super$update;
+    Fires.create(Vars.world.tileWorld(unit.x,unit.y));
     print(unit.x);
     };
   }
