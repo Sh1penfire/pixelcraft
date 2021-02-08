20a5cb50cecbd519857e68b2fa085f97a7c5603a
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2021-02-08 22:46:13 +1100

    pleawse be fixed

--- scripts/blocks/environment.js
+++ scripts/blocks/environment.js
@@ -12,9 +12,19 @@ voidicsm.rust,
 voidicsm.stone
 ]
 ///havin some fun in code, so no
+/*and apparently it no work
 let no = 0
 blocks.each(e => {
     blocks[no].itemDrop = itemDrop[no]
     no++
     e.playerUnmineable = true
 });
+*/
+blocks[0].playerUnmineable = true
+blocks[0].itemDrop = itemDrop[0]
+blocks[1].playerUnmineable = true
+blocks[1].itemDrop = itemDrop[1]
+blocks[2].playerUnmineable = true
+blocks[2].itemDrop = itemDrop[2]
+blocks[3].playerUnmineable = true
+blocks[3].itemDrop = itemDrop[3]
