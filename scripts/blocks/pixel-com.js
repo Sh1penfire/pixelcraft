564ffef21ffb03e8e62493e913f0af35566a4a2c
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2021-02-10 10:56:39 +1100

    fixing things I hop

--- scripts/blocks/pixel-com.js
+++
@@ -1,7 +0,0 @@
-const pixelframes = new DrawAnimation();
-
-pixelframes.frameCount = 5;
-
-const pixelcompress = extendContent(GenericCrafter,"pixelite-compresser",{});
-
-pixelcompress.drawer = pixelframes;
