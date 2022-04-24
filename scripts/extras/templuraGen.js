3ce7a9532e70a7540c924c957508823da7d04450
Sh1p*nfire <73347888+Sh1penfire@users.noreply.github.com>
2022-04-25 01:15:16 +1000

    why was this commented out?

--- scripts/extras/templuraGen.js
+++ scripts/extras/templuraGen.js
@@ -1,4 +1,4 @@
-/*const environment = require("blocks/environment");
+const environment = require("blocks/environment");
 const weathers = require("extras/weathers");
 const pWaves = require("units/pWaves");
 
@@ -401,4 +401,3 @@ templuraGenerator.water = 2 / templuraGenerator.arr[0].length;
 module.exports = {
     templura: templuraGenerator
 };
-*/
